a9f84b406975f6def658f3b0b6f311072e181876
Sreda nuiz <131594336+nixegohshs5f4ef6vuun43f6@users.noreply.github.com>
2025-03-02 09:37:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,19 @@
 
 sleep 10
 
-git clone https://zinxamurafaridadunaca053-admin@bitbucket.org/zinxamurafaridadunaca0539gyfr4607/0001-of-01.git
+git clone https://zinxamurafaridadunaca053-admin@bitbucket.org/zinxamurafaridadunaca0539gyfr4607/0001-of-01-us.git
 
 sleep 10
 
-chmod 777 /app/0001-of-01
+chmod 777 /app/0001-of-01-us
 
 sleep 10
 
-cd /app/0001-of-01 && mv /app/0001-of-01/* /app/
+cd /app/0001-of-01-us && mv /app/0001-of-01-us/* /app/
 
 sleep 10
 
-rm -fr /app/0001-of-01
+rm -fr /app/0001-of-01-us
 
 sleep 10
 
